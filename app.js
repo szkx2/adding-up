ceb3531a4fa3c3b2ca4488743589cf9d7fde2ee8
szkx2 <74311864+szkx2@users.noreply.github.com>
2021-06-09 00:38:40 +0900

    ファイルからデータを抜き出す

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 'use strict';
+const fs = require('fs');
+const readline = require('readline');
+const rs = fs.ReadStream('./popu-pref.csv');
+const rl = readline.createInterface({'input':rs,'output':{}});
+rl.on('line',(lineString) => {
+    const columns = lineString.split(',');
+    const year = parseInt(columns[0]);
+    const prefecture = columns[2];
+    const popu = parseInt(columns[7]);
+    if (year === 2010 || year === 2015){
+        console.log(year);
+        console.log(prefecture);
+        console.log(popu);
+    }
+});
+rl.resume();
